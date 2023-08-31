530aa46e9aeb0928b6d93cdc984708518230244f
FreitasRj1 <rm550187@fiap.com.br>
2023-08-31 14:46:35 -0300

    ssss

--- SRC/Pages/login.js
+++ SRC/Pages/login.js
@@ -1,31 +1,24 @@
-function validar(){
-let usuario = document.getElementById("name").value
-let senha = document.getElementById("password").value
-let email = document.getElementById('email').value
-
-
-
-
-if (usuario =="" && senha =="" && email ==""){
-    alert('Preencha os campos')
-}
-
-if (email ==""){
-    alert('Por favor insira seu email')
-} 
-if (senha==""){
-    alert('É obrigatorio o uso de senha para cadastrar-se em nosso site')
-}else if(senha == email){
-    alert('Não e permitido a senha e o email serem iguais')
-}else if(usuario==""){
-    alert('Preencha o campo "nome"')
-}
-else{
-    alert('Login efetuado com sucesso')
-    
-}
-
-}
-if (window.Map ==('index.html')){
-    alert('Bem vindo a nossa homepage')
-}
+function validar() {
+    let usuario = document.getElementById("user").value;
+    let senha = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+  
+    if (usuario === "" || senha === "" || email === "") {
+      alert('Preencha todos os campos');
+    } else if (email === "") {
+      alert('Por favor, insira seu email');
+    } else if (senha === "") {
+      alert('É obrigatório o uso de senha para cadastrar-se em nosso site');
+    } else if (usuario === "") {
+      alert('Preencha o campo "nome"');
+    } else {
+      alert('Login efetuado com sucesso');
+      window.location.href = "principal.html"; // Redireciona para a página principal.html
+    }
+  }
+  
+  window.onload = function() {
+    if (window.location.href.includes('principal.html')) {
+      alert('Bem-vindo à nossa homepage');
+    }
+  }
